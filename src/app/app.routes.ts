2a08c4d08323bc5681da3e86e422f2fdcfe5d79f
Karine <karine.avdalyan05@gmail.com>
2026-01-07 17:46:14 +0400

    login guard added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Dashboard} from './pages/dashboard/dashboard';
+import { Dashboard } from './pages/dashboard/dashboard';
 import { Citizens } from './features/citizens/citizens';
 import { Programs } from './features/programs/programs';
 import { Applications } from './features/applications/applications';
@@ -9,32 +9,21 @@ import { RolesComponent } from './features/security/roles/roles.component';
 import { UsersComponent } from './features/security/users/users.component';
 import { SetPasswordComponent } from './pages/auth/set-password/set-password.component';
 
+import { authGuard } from './core/guards/auth.guard';
+
 export const routes: Routes = [
   {
     path: '',
     component: Dashboard,
+    canActivate: [authGuard],
     children: [
-      {
-        path: 'citizens', component: Citizens
-      },
-      {
-        path: 'programs', component: Programs
-      },
-      {
-        path: 'applications', component: Applications
-      },
-      {
-        path: 'analytics', component: Analytics
-      },
-      {
-        path: '', redirectTo: 'analytics', pathMatch: 'full'
-      },
-      {
-        path: 'roles', component: RolesComponent
-      },
-      {
-        path: 'users', component: UsersComponent
-      }
+      { path: 'citizens', component: Citizens },
+      { path: 'programs', component: Programs },
+      { path: 'applications', component: Applications },
+      { path: 'analytics', component: Analytics },
+      { path: 'roles', component: RolesComponent },
+      { path: 'users', component: UsersComponent },
+      { path: '', redirectTo: 'analytics', pathMatch: 'full' }
     ]
   },
 
@@ -42,6 +31,7 @@ export const routes: Routes = [
     path: 'login',
     component: LoginComponent
   },
+
   {
     path: 'set-password',
     component: SetPasswordComponent
